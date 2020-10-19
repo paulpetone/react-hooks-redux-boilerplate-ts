d6034b4ee0408ae78037bf94d817a54788ad43cf
Paulo Henrique Silveira <paulohsilveira@outlook.com>
2020-10-19 17:03:46 -0300

    About composeHooks

--- src/helpers/composeHooks.tsx
+++ src/helpers/composeHooks.tsx
@@ -10,3 +10,33 @@ export default (...layers: Array<Function>) => (BaseComponent: Function) => {
       return <BaseComponent {...props} {...hooksProps} />
     }
   }
+
+/*
+This helper allows u to compose your hooks, passing to the next layer your old props and the new ones!
+It always should be a function that returns a object. Example:
+
+> component/hooks/index.tsx
+
+import composeHooks from '@helpers/composeHooks'
+
+export default composeHooks(
+  () => {
+    return {
+      newProp: 'hi, i'm a new prop'
+    }
+  },
+  ({ newProp }) => {
+    console.log(newProp, 'I got this prop from the previous hook')
+    return {}
+  }
+)
+
+
+> component/index.tsx
+
+import enhancer from './hooks'
+
+const myComponent = () => <>hi all</>
+
+export default enhancer(myComponent)
+*/
