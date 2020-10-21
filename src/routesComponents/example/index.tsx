00bfa83958c58ba64c7385011bd856a1cdd460e7
Paulo Henrique Silveira <paulohsilveira@outlook.com>
2020-10-21 13:57:05 -0300

    Build test

--- src/routesComponents/example/index.tsx
+++ src/routesComponents/example/index.tsx
@@ -1,4 +1,4 @@
-import ExamplesOfUsage from './components/ExamplesOfUsage'
+import ExamplesOfUsage from '@routesComponents/example/components/ExamplesOfUsage'
 import Layout from '@components/layout'
 import SEO from '@components/seo'
 
