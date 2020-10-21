c9ba3e5fb60e028e2cb3f3faab9b26b0d38ab8a1
Paulo Henrique Silveira <paulohsilveira@outlook.com>
2020-10-21 13:44:00 -0300

    Updating project name

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `react-hooks-redux-boilerplate-ts`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
